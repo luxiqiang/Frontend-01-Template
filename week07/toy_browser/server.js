f9898c13967b636766c8828eff3d507d1d2ce2f8
luxiqiang <luxiqiang19920213@gmail.com>
2020-05-27 23:37:47 +0800

    homework

---
+++ week07/toy_browser/server.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log('request received');
+  console.log(req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('X-Foo', 'bar');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`
+<html maaa=a >
+<head>
+<style>
+#wrap {
+  display: flex;
+  width: 600px;
+  height: 600px;
+  background-color: #fff;
+}
+.item1 {
+  width: 200px;
+  height: 300px;
+  background-color: #000;
+}
+.item12{
+  flex: 1
+  height: 400px;
+  background-color: green;
+}
+</style>
+</head>
+<body>
+  <div id="wrap">
+    <div class="item1">item1</div>
+    <div class="item2">item2</div>
+  </div>
+</body>
+</html>
+  `);
+})
+
+server.listen(8088);
